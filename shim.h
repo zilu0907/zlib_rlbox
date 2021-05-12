83a0ff3043534500b5e30598fac6d11ef1b03d58
毛郁菲 <739093828@qq.com>
2021-05-12 15:31:56 -0700

    init

---
+++ shim.h
@@ -0,0 +1,5 @@
+#define RLBOX_USE_STATIC_CALLS() rlbox_noop_sandbox_lookup_symbol
+
+#include "rlbox_sandboxing_api/code/include/rlbox.hpp"
+#include "rlbox_sandboxing_api/code/include/rlbox_noop_sandbox.hpp"
+
