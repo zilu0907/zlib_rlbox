cbb7d51eafb8450eeaaacba5e724d7052c14704c
毛郁菲 <739093828@qq.com>
2021-05-15 18:00:11 -0700

    add cmake

---
+++ build/CMakeFiles/zpipe_rlbox.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zpipe_rlbox.dir/zpipe_verify.c.o"
+  "zpipe_rlbox"
+  "zpipe_rlbox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zpipe_rlbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- shim.h
+++
@@ -1,5 +0,0 @@
-#define RLBOX_USE_STATIC_CALLS() rlbox_noop_sandbox_lookup_symbol
-
-#include "rlbox_sandboxing_api/code/include/rlbox.hpp"
-#include "rlbox_sandboxing_api/code/include/rlbox_noop_sandbox.hpp"
-
